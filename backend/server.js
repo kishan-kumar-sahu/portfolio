4b81798f78574073f7a771ded98d187e1216ac00
kishan kumar sahu <kumarkishan26636@gmail.com>
2026-01-01 23:12:25 +0530

    Fix Express v5 wildcard route for React Router

--- backend/server.js
+++ backend/server.js
@@ -115,10 +115,9 @@ const frontendDistPath = path.join(__dirname, "../frontend/dist");
 
 app.use(express.static(frontendDistPath));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(frontendDistPath, "index.html"));
 });
-
 
 
 
